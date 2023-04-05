0597c738737a3f3c6b6cefb29287806ccfc14075
annavrbaski <69722848+annavrbaski@users.noreply.github.com>
2023-04-05 16:38:31 -0400

    added test graph
    added a temp graph to device

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -7,7 +8,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->test, SIGNAL(released()), this, SLOT (test()));
+    //connect(ui->test, SIGNAL(released()), this, SLOT (test()));
+    test();
 }
 
 MainWindow::~MainWindow()
@@ -17,6 +19,31 @@ MainWindow::~MainWindow()
 
 void MainWindow::test()
 {
+    // add temp graph
+    qInfo("test");
+
+    QCustomPlot *customPlot = ui->Graph;
+    customPlot->addGraph();
+    customPlot->graph(0)->setPen(QPen(Qt::blue));
+
+    QVector<double> x(251), y(251);
+    for (int i=0; i < 251; ++i)
+    {
+        x[i] = i;
+        y[i] = qExp(-i/150.0)*qCos(i/10.0);
+    }
+
+    customPlot->xAxis2->setVisible(false);
+    customPlot->xAxis2->setTickLabels(false);
+    customPlot->yAxis2->setVisible(false);
+    customPlot->yAxis2->setTickLabels(false);
+    customPlot->xAxis->setTickLabels(false);
+    customPlot->yAxis->setTickLabels(false);
+
+    connect(customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->xAxis2, SLOT(setRange(QCPRange)));
+    connect(customPlot->yAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->yAxis2, SLOT(setRange(QCPRange)));
+    customPlot->graph(0)->setData(x, y);
+    customPlot->rescaleAxes();
 
 }
 
